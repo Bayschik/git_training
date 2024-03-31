6796460b618935aec89ff4f6979323a879936aa4
Baychik <ergeshovbaistan@gmail.com>
2024-03-31 12:14:37 +0600

    Первый коммит

---
+++ app/src/main/java/com/example/git_training/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.git_training
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.git_training.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
