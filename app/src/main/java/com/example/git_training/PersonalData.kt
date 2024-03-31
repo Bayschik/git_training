8457d71cae8c03fd65fef9b3e03c2fdba676e3e8
Baychik <ergeshovbaistan@gmail.com>
2024-03-31 13:54:57 +0600

    Камола исправила некоторую часть своего кода

--- app/src/main/java/com/example/git_training/PersonalData.kt
+++ app/src/main/java/com/example/git_training/PersonalData.kt
@@ -2,6 +2,6 @@ package com.example.git_training
 
 class PersonalData {
     fun main(){
-        println("THIS IS THE PROFILE FRAGMENT")
+        println("THIS IS THE PROFILE FRAGMEыодыаоNT")
     }
 }
